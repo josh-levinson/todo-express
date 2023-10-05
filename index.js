6af2edaab51bac1103ceeac59ca6ba17ce626eaf
Josh Levinson <joshua.levinson@gmail.com>
2023-10-05 18:45:16 -0400

    use cors + port 3002 by default

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 
@@ -10,18 +11,19 @@ mongoose.connect(mongoString);
 const database = mongoose.connection
 
 database.on('error', (error) => {
-    console.log(error)
+  console.log(error)
 });
 
 database.once('connected', () => {
-    console.log('Database Connected');
+  console.log('Database Connected');
 });
 
 const app = express ();
 app.use(express.json());
+app.use(cors());
 app.use('/api', routes)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
